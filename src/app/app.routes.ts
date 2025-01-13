9366dac427cc838b1bfcd807aca75dfad4792a53
Anthonazo <anthonymoya246@gmail.com>
2025-01-13 17:11:04 -0500

    creacion de interfaz, se crea la estructura y componentes basicos sin funcionalidad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'home', component: MainComponent},
+    {path: 'login', component: AuthComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '**', redirectTo: 'home', pathMatch: 'full'}
+    
+];
---
+++ src/app/layouts/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  isLogged = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.isLogged = localStorage.getItem('logged') ? true : false;
+  }
+  
+
+}
