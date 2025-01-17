3cca565cbb142780f3e084a7ae35b1e1f129ac3d
Anthonazo <anthonymoya246@gmail.com>
2025-01-17 03:31:37 -0500

    se crea apartado de subida de imagenes con vista previa en base64

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layouts/header/header.component";
 import { FooterComponent } from "./layouts/footer/footer.component";
 import { HttpClient } from '@angular/common/http';
@@ -7,10 +7,21 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'Frontend_Alzheimer_Prediction';
+
+  // constructor(private router: Router) {}
+
+  // ngOnInit(): void {
+  //   if(localStorage.getItem('logged')){
+  //     this.router.navigate(['/home']);
+  //   } else {
+  //     this.router.navigate(['/login']);
+  //   }
+  // }
 }
--- src/app/pages/uploadimg/uploadimg.component.ts
+++ src/app/pages/uploadimg/uploadimg.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-uploadimg',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './uploadimg.component.html',
   styleUrl: './uploadimg.component.scss'
 })
 export class UploadimgComponent {
+
+  selectedFile: any = null;
+  imagePreview: any = null;
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0]; // Obtiene el archivo seleccionado
+      const reader = new FileReader();
+
+      // Genera una vista previa de la imagen
+      reader.onload = () => {
+        this.imagePreview = reader.result as string;
+        console.log(this.imagePreview);
+      };
+
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+
+  uploadImage() {
+    
+
+  }
 
 }
